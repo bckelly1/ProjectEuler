68ffa95b2aed28a2f5207d5a73261711ba342b0b
Brian Kelly <Brian.Kelly@ca.com>
2017-09-13 14:12:37 -0600

    adding new result

--- src/main/java/Problem14.java
+++ src/main/java/Problem14.java
@@ -1,54 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by brian on 7/29/15.
  */
 public class Problem14 {
-    static int longest = 0;
-    static List<Integer> chain = new ArrayList<>();
-    public static void main(String[] args){
-        int starter;
-        int cycles = 0;
-        for(int i = 0; i < 1000000; i++){
-            List<Integer> this_chain = new ArrayList<>();
-            starter = i;
-            while(starter > 0) {
-                cycles++;
-                this_chain.add(starter);
-                if (starter % 2 == 0) {
-                    starter = even_collatz(starter);
+
+    public static void main(String[] args) {
+        int number = 1000000;
+
+        long sequenceLength = 0;
+        long startingNumber = 0;
+        long sequence;
+
+        for (int i = 2; i <= number; i++) {
+            int length = 1;
+            sequence = i;
+            while (sequence != 1) {
+                if ((sequence % 2) == 0) {
+                    sequence = sequence / 2;
                 } else {
-                    starter = odd_collatz(starter);
+                    sequence = sequence * 3 + 1;
                 }
-                if(starter == 1){
-                    this_chain.add(starter);
-                    cycles++;
-                    break;
-                }
+                length++;
             }
-            if (cycles > longest){
-                longest = cycles;
-                chain = this_chain;
-            }
-            cycles = 0;
-        }
-        System.out.println("Longest: " + longest);
-        System.out.println("Chain: ");
-        int c = 0;
-        for(int index : chain){
-            System.out.print(index + " ");
-            c++;
-            if(c % 20 == 0){
-                System.out.println();
+
+            if (length > sequenceLength) {
+                sequenceLength = length;
+                startingNumber = i;
             }
         }
-        System.out.println();
-    }
-    static int even_collatz(int input){
-        return input/2;
-    }
-    static int odd_collatz(int input){
-        return (3*input)+1;
+
+        System.out.println(sequenceLength);
+        System.out.println(startingNumber);
     }
 }
